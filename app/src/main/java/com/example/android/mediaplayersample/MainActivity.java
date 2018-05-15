bc74763e570a4054131d70613512635e5f428532
Hudson Pereira <hudson.byte@gmail.com>
2018-05-15 15:28:52 -0300

    add audio and audio controls

--- app/src/main/java/com/example/android/mediaplayersample/MainActivity.java
+++ app/src/main/java/com/example/android/mediaplayersample/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.android.mediaplayersample;
 
 import android.media.MediaPlayer;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -14,14 +16,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mediaPlayer = new MediaPlayer();
+        mediaPlayer = MediaPlayer.create(this, R.raw.superboy);
     }
 
     public void onPlayClicked(View view) {
         mediaPlayer.start();
     }
 
-    onPauseClicked(View view) {
+    public void onPauseClicked(View view) {
         mediaPlayer.pause();
     }
 }
